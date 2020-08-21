43ca4ea47cd9db069adc28803fb1d7c4fabd01c7
Alex Ackerlund <alexackerlund@bresnan.net>
2020-08-21 10:16:13 -0600

    Added multithreading

--- project1/README.md
+++ project1/README.md
@@ -5,3 +5,10 @@ Some things to note
         1. ex: valid data points are between 0 and 10, 90% of those values are between 2 and 3, so we generate a random value that is weighted to be between 2 and 3.
     3. either way we need to document our choice.
         1. I think we should go with random values to start and then later we can try and use seemingly accurate values.
+2. This program is multithreaded
+    1. This should cut down on runtime significantly (Assuming this program takes as long as it has in previous semesters).
+    2. To accomplish this you do the following:
+        1. Extend Thread in the class you want to multithread
+        2. Call [class instance].start()
+        3. in [class] make a ``` public void run() ``` function.
+            1. This is what [class instance].start() calls
--- project1/src/Driver.java
+++ project1/src/Driver.java
@@ -3,11 +3,16 @@ import Nodes.*;
 import java.util.ArrayList;
 import java.util.Arrays;//used in printing out the parsed data
 
-public class Driver
+public class Driver extends Thread//extending Thread allows for multithreading
 {
-	String fileStart = "dataSets/", fileEnd = ".data";
+	String fileStart = "dataSets/", fileEnd = ".data", filePath;
 	
 	public Driver(String filePath)
+	{
+		this.filePath = filePath;
+	}
+		
+	public void run()//the method that is called when a Thread starts
 	{
 		//parse out the data in the file
 		System.out.println(filePath);
@@ -16,35 +21,40 @@ public class Driver
 		{
 			case "house-votes-84":
 				ArrayList<VoteNode> vNodes = p.votesParser(fileStart + filePath + fileEnd);
-				/*for(VoteNode node : vNodes)
+				/*System.out.println("Done 1");
+				for(VoteNode node : vNodes)
 				{
 					System.out.println(node.getDR() + " " + Arrays.toString(node.getVotes()));
 				}*/
 				break;
 			case "breast-cancer-wisconsin":
 				ArrayList<CancerNode> cNodes = p.cancerParser(fileStart + filePath + fileEnd);
-				/*for(CancerNode node : cNodes)
+				/*System.out.println("Done 2");
+				for(CancerNode node : cNodes)
 				{
 					System.out.println(node.getId() + Arrays.toString(node.getData()));
 				}*/
 				break;
 			case "glass":
 				ArrayList<GlassNode> gNodes = p.glassParser(fileStart + filePath + fileEnd);
-				/*for(GlassNode node : gNodes)
+				/*System.out.println("Done 3");
+				for(GlassNode node : gNodes)
 				{
 					System.out.println(node.getId() + Arrays.toString(node.getData()) + node.getGlassType());
 				}*/
 				break;
 			case "iris":
 				ArrayList<IrisNode> iNodes = p.irisParser(fileStart + filePath + fileEnd);
-				/*for(IrisNode node : iNodes)
+				/*System.out.println("Done 4");
+				for(IrisNode node : iNodes)
 				{
 					System.out.println(node.getType() + Arrays.toString(node.getData()));
 				}*/
 				break;
 			case "soybean-small":
 				ArrayList<BeanNode> bNodes = p.beanParser(fileStart + filePath + fileEnd);
-				/*for(BeanNode node : bNodes)
+				/*System.out.println("Done 5");
+				for(BeanNode node : bNodes)
 				{
 					System.out.println(node.getType() + Arrays.toString(node.getData()));
 				}*/
@@ -52,15 +62,16 @@ public class Driver
 			default:
 				System.out.println("Bad file path: " + filePath);
 		}
-		
 	}
+	
 	public static void main(String[] args)
 	{
 		String[] files = {"breast-cancer-wisconsin", "glass", "house-votes-84", "iris", "soybean-small"};
 		
-		for(String file : files)//create a new instance of the driver for each of the data sets. TODO multi thread this to cut down on runtime.
+		for(String file : files)//create a new instance of the driver for each of the data sets.
 		{
-			new Driver(file);
+			Driver d = new Driver(file);
+			d.start();//Starts a new thread
 		}
 	}
 }
