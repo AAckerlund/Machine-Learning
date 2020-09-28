5a4974d0dcf307919f8b117eb732a69f89b3a9ec
Alex Ackerlund <alexackerlund@bresnan.net>
2020-09-28 10:34:35 -0600

    finished parsing

--- project2/src/Driver.java
+++ project2/src/Driver.java
@@ -28,30 +28,39 @@ public class Driver extends Thread//extending Thread allows for multithreading
 				numattrValues = 2;
 				System.out.println("Done Votes");
 			}
-			case "breast-cancer-wisconsin" -> {
-				nodes = p.cancerParser(fileStart + filePath + fileEnd);
-				System.out.println("Done Cancer");
-			}
 			case "glass" -> {
 				nodes = p.glassParser(fileStart + filePath + fileEnd);
 				System.out.println("Done Glass");
 			}
-			case "iris" -> {
-				nodes = p.irisParser(fileStart + filePath + fileEnd);
-				System.out.println("Done Iris");
+			case "abalone" -> {
+				nodes = p.abaloneParser(fileStart + filePath + fileEnd);
+				System.out.println("Done Abalone");
 			}
-			case "soybean-small" -> {
-				nodes = p.beanParser(fileStart + filePath + fileEnd);
-				attrValueLow = 0;
-				System.out.println("Done Beans");
+			case "forestfires" -> {
+				nodes = p.firesParser(fileStart + filePath + fileEnd);
+				System.out.println("Done Forest Fires");
+			}
+			case "machine" -> {
+				nodes = p.machineParser(fileStart + filePath + fileEnd);
+				System.out.println("Done Machine");
+			}
+			case "segmentation" -> {
+				nodes = p.segmentationParser(fileStart + filePath + fileEnd);
+				System.out.println("Done Segmentation");
 			}
 			default -> System.out.println("Bad file path: " + filePath);
 		}
 		
+		//print data to verify
+		/*for(Node node : nodes)
+			System.out.println(node.getId() + " " + Arrays.toString(node.getData()));
+		
+		/*
 		BayesNet(nodes, attrValueLow, numattrValues);//runs on the data as it appears in the .data files.
 		DataShuffler.shuffleFeatureData(nodes);	//Shuffle one attribute
 		System.out.println("Running shuffled data");
 		BayesNet(nodes, attrValueLow, numattrValues);//runs on the data that has had one attribute shuffled.
+		 */
 	}
 	
 	public void BayesNet(ArrayList<Node> nodes, int attrValueLow, int numattrValues)
@@ -107,38 +116,18 @@ public class Driver extends Thread//extending Thread allows for multithreading
 	}
 
 	public static void main(String[] args) {
-		String[] files = {"house-votes-84", "breast-cancer-wisconsin", "glass", "iris", "soybean-small"};
+		String[] files = {"abalone", "forestfires", "glass", "house-votes-84", "machine", "segmentation"};
 		
+		//use these if you want to run a single data set
+		//Driver test = new Driver(files[5]);
+		//test.start();
+		
+		//use these if you want to run all the data sets
 		for (String file : files)//create a new instance of the driver for each of the data sets.
 		{
 			Driver d = new Driver(file);
-			System.out.println("\n********************\n" + file + "\n********************\n");
-			d.run();//Starts a new thread
+			//System.out.println("\n********************\n" + file + "\n********************\n");
+			d.start();//Starts a new thread
 		}
-
-		/*
-		Driver house_votes_driver = new Driver("house-votes-84");
-		System.out.println("House Votes Thread: ");
-		house_votes_driver.start();
-*/
-		/*
-		Driver breast_cancer_wisconsin_driver = new Driver("breast-cancer-wisconsin");
-		System.out.println("Breast Cancer Thread: ");
-		breast_cancer_wisconsin_driver.start();
-		*/
-		/*
-		Driver glass_driver = new Driver("glass");
-		System.out.println("Glass Thread: ");
-		glass_driver.start();
-		*/
-		/*
-		Driver iris_driver = new Driver("iris");
-		System.out.println("Iris Thread: ");
-		iris_driver.start();
-		*/
-		/*Driver soybean_small_driver = new Driver("soybean-small");
-		System.out.println("Soybean Thread: ");
-		soybean_small_driver.start();*/
-		
 	}
 }
--- project2/src/Parser.java
+++ project2/src/Parser.java
@@ -3,6 +3,7 @@ import MinMax.IrisDataMinMax;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Parser
@@ -12,7 +13,6 @@ public class Parser
 	 * @param filePath the path to the file the scanner is attached to.
 	 * @return an initialized scanner, or null if the scanner failed to initialize.
 	 */
-
 	public Scanner initScanner(String filePath)
 	{
 		File file = new File(filePath);
@@ -42,76 +42,24 @@ public class Parser
 	}
 	
 	//The below functions are all slightly different but all parse the data out from their respective files.
-	public ArrayList<Node> beanParser(String filePath)
-	{
-		Scanner in = initScanner(filePath);
-		ArrayList<Node> nodes = new ArrayList<>();
-		while(in.hasNext())
-		{
-			String line = in.next();
-			String[] data = line.split(",");
-			
-			float type = Integer.parseInt(data[data.length-1].substring(1));
-			float[] dataPoints = new float[data.length-1];
-			for(int i = 0; i < data.length - 1; i++)
-			{
-				dataPoints[i] = Integer.parseInt(data[i]);
-			}
-			nodes.add(new Node(type, dataPoints));
-		}
-		return nodes;
-	}
-	
-	public ArrayList<Node> irisParser(String filePath)
-	{
-		Scanner in = initScanner(filePath);
-		ArrayList<Node> nodes = new ArrayList<>();
-		while(in.hasNext())
-		{
-			String line = in.next();
-			String[] data = line.split(",");
-			
-			String typeString = data[data.length-1].substring(5);
-			float type;
-			switch(typeString)
-			{
-				case "setosa" -> type = 1;
-				case "versicolor" -> type = 2;
-				case "virginica" -> type = 3;
-				
-				default -> { //unknown type or an error happened
-					type = (int) (Math.random() * 3) + 1;
-					System.out.println("iris " + type);
-				}
-			}
-			float[] dataPoints = new float[data.length-1];
-			for(int i = 0; i < data.length-1; i++)
-			{
-				dataPoints[i] = Float.parseFloat(data[i]);
-			}
-			nodes.add(new Node(type, dataPoints));
-		}
-		Discretizer irisDiscretizer = new Discretizer(10);
-		IrisDataMinMax irisstats = new IrisDataMinMax();
-		double [][] minmax = irisstats.getGlassMinMaxList();
-		nodes = discreteParser(irisDiscretizer, nodes, minmax);
-		return nodes;
-	}
-	
 	public ArrayList<Node> glassParser(String filePath)
 	{
 		Scanner in = initScanner(filePath);
 		ArrayList<Node> nodes = new ArrayList<>();
 		while(in.hasNext())
 		{
+			//read in one element of the data set
 			String line = in.next();
 			String[] data = line.split(",");
+			
+			//try and set the id. If we cant then set it to 0.
 			float type = 0;
 			try
 			{
 				type = Integer.parseInt(data[data.length-1]);
 			}
 			catch(NumberFormatException ignored){}
+			
 			
 			float[] dataPoints = new float[data.length-2];
 			for(int i = 1; i < data.length-1; i++)
@@ -127,36 +75,6 @@ public class Parser
 		nodes = discreteParser(glassDiscretizer, nodes, minmax);
 		return nodes;
 	}
-	
-	public ArrayList<Node> cancerParser(String filePath)
-	{
-		Scanner in = initScanner(filePath);
-		ArrayList<Node> nodes = new ArrayList<>();
-		while(in.hasNext())
-		{
-			String line = in.next();
-			String[] data = line.split(",");
-			
-			float id = Integer.parseInt(data[data.length-1]);
-			
-			float[] dataPoints = new float[data.length-2];
-			for(int i = 1; i < data.length-1; i++)
-			{
-				try
-				{
-					dataPoints[i - 1] = Integer.parseInt(data[i]);
-				}
-				catch(NumberFormatException ex)
-				{
-					dataPoints[i-1] = (int)(Math.random()*10) + 1;
-				}
-			}
-			nodes.add(new Node(id, dataPoints));
-		}
-
-		return nodes;
-	}
-	
 	public ArrayList<Node> votesParser(String filePath)
 	{
 		Scanner in = initScanner(filePath);
@@ -175,7 +93,7 @@ public class Parser
 			float[] votes = new float[data.length-1];
 			for(int i = 1; i < data.length; i++)
 			{
-				switch(data[i])
+				switch(data[i])//attempt to set a value for each vote. If not possible, randomize the value
 				{
 					case ("y") -> votes[i - 1] = 1;
 					case ("n") -> votes[i - 1] = 2;
@@ -183,6 +101,149 @@ public class Parser
 				}
 			}
 			nodes.add(new Node(dr, votes));
+		}
+		return nodes;
+	}
+	public ArrayList<Node> abaloneParser(String filePath)
+	{
+		Scanner in = initScanner(filePath);
+		ArrayList<Node> nodes = new ArrayList<>();
+		while(in.hasNext())
+		{
+			String line = in.next();
+			String[] data = line.split(",");
+			/*
+			male = 0
+			female = 1
+			infant = 2
+			 */
+			float id;
+			if(data[0].equals("M"))
+				id = 0;
+			else if(data[0].equals("F"))
+				id = 1;
+			else//data[0].equals("I")
+				id = 2;
+			
+			float[] attributes = new float[data.length-1];
+			for(int i = 0; i < attributes.length; i++)
+			{
+				attributes[i] = Float.parseFloat(data[i+1]);
+			}
+			
+			nodes.add(new Node(id, attributes));
+		}
+		return nodes;
+	}
+	public ArrayList<Node> firesParser(String filePath)
+	{
+		Scanner in = initScanner(filePath);
+		ArrayList<Node> nodes = new ArrayList<>();
+		String line = in.next();//the first line is not data so skip
+		
+		//setting up some hashmaps for fast data parsing
+		HashMap<String, Integer> months = new HashMap<>();
+		months.put("jan", 0);
+		months.put("feb", 1);
+		months.put("mar", 2);
+		months.put("apr", 3);
+		months.put("may", 4);
+		months.put("jun", 5);
+		months.put("jul", 6);
+		months.put("aug", 7);
+		months.put("sep", 8);
+		months.put("oct", 9);
+		months.put("nov", 10);
+		months.put("dec", 11);
+		
+		HashMap<String, Integer> days = new HashMap<>();
+		days.put("mon", 0);
+		days.put("tue", 1);
+		days.put("wed", 2);
+		days.put("thu", 3);
+		days.put("fri", 4);
+		days.put("sat", 5);
+		days.put("sun", 6);
+		
+		while(in.hasNext())
+		{
+			line = in.next();
+			String[] data = line.split(",");
+			
+			float[] attributes = new float[data.length];
+			
+			attributes[2] = months.get(data[2]);
+			attributes[3] = days.get(data[3]);
+			
+			attributes[0] = Float.parseFloat(data[0]);
+			attributes[1] = Float.parseFloat(data[1]);
+			for(int i = 4; i < attributes.length; i++)
+			{
+				attributes[i] = Float.parseFloat(data[i]);
+			}
+			nodes.add(new Node(0, attributes));
+		}
+		return nodes;
+	}
+	public ArrayList<Node> machineParser(String filePath)
+	{
+		Scanner in = initScanner(filePath);
+		ArrayList<Node> nodes = new ArrayList<>();
+		
+		HashMap<String, Integer> vendor = new HashMap<>();
+		HashMap<String, Integer> model = new HashMap<>();
+		while(in.hasNext())
+		{
+			String line = in.next();
+			String[] data = line.split(",");
+			float[] attributes = new float[data.length];
+			for(int i = 2; i < attributes.length; i++)
+			{
+				attributes[i] = Float.parseFloat(data[i]);
+			}
+			
+			//due to the lengthy list of possible values we dynamically add them to a hashmap
+			if(vendor.get(data[0]) == null)
+				vendor.put(data[0], vendor.size());
+			attributes[0] = vendor.get(data[0]);
+			//System.out.print(attributes[0] + " ");
+			
+			if(model.get(data[1]) == null)
+				model.put(data[1], model.size());
+			attributes[1] = model.get(data[1]);
+			//System.out.println(attributes[1]);
+			
+			nodes.add(new Node(0, attributes));
+		}
+		return nodes;
+	}
+	public ArrayList<Node> segmentationParser(String filePath)
+	{
+		Scanner in = initScanner(filePath);
+		ArrayList<Node> nodes = new ArrayList<>();
+		
+		for(int i = 0; i < 4; i++)//first few lines are not data, so skip them
+		{
+			in.nextLine();
+		}
+		
+		HashMap<String, Integer> classes = new HashMap<>();
+		while(in.hasNext())
+		{
+			String line = in.next();
+			String[] data = line.split(",");
+			float id;
+			float[] attributes = new float[data.length-1];
+			
+			if(classes.get(data[0]) == null)
+				classes.put(data[0], classes.size());
+			id = classes.get(data[0]);
+			
+			for(int i = 1; i < data.length; i++)
+			{
+				attributes[i-1] = Float.parseFloat(data[i]);
+			}
+			nodes.add(new Node(id, attributes));
 		}
 		return nodes;
 	}
