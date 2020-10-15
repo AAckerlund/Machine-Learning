4a38589d5345fd98034c7e88eb0366c6fd12e5ef
Alex Ackerlund <alexackerlund@bresnan.net>
2020-10-15 17:43:29 -0600

    more functionality for Neuron

--- project3/src/Neuron.java
+++ project3/src/Neuron.java
@@ -1,14 +1,44 @@
 import java.util.ArrayList;
-
+/*
+edge weights are initialized to random
+each node points to every node in the next layer
+ */
 public class Neuron
 {
 	private ArrayList<Neuron> outputs;
 	private ArrayList<Double> weights;
-	double biasWeight;
+	private double biasWeight;
+	private double value;
 	
 	public Neuron()
 	{
 		outputs = new ArrayList<>();
 		weights = new ArrayList<>();
+		biasWeight = 0;
+		value = 0;
+	}
+	
+	public void updateValue(double inputValue)
+	{
+		value += inputValue;
+	}
+	
+	public ArrayList<Double> calcOutput()
+	{
+		ArrayList<Double> outputValues = new ArrayList<>();
+		value = Activation.Sigmoidal(weights, outputValues);
+		for(int i = 0; i < outputs.size(); i++)
+		{
+			outputValues.add(value*weights.get(i));
+		}
+		return outputValues;
+	}
+	
+	public void pushUpdate(ArrayList<Double> valueUpdates)
+	{
+		for(int i = 0; i < valueUpdates.size(); i++)
+		{
+			outputs.get(i).updateValue(valueUpdates.get(i));
+		}
 	}
 }
