ee1ec512ea2c9e282a889bab717027e9008e16fa
Alex Ackerlund <alexackerlund@bresnan.net>
2020-11-11 10:56:06 -0700

    updates for bug fixes

--- project 4/src/Driver.java
+++ project 4/src/Driver.java
@@ -2,13 +2,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/*
+TODO
+Modify NN to use chromosomes - Alex
+Select() - Elijah (Alex join when done other stuff)
+crossover() - Elijah (Alex join when done other stuff)
+mutate() - Elijah  (Alex join when done other stuff)
+replacement() - Elijah (Alex join when done other stuff)
+Particle datastructure - Jon
+Particle Update - Jon
+Cross entropy for classification - Elijah (pull from p3)
+chromosome datastructure - Alex
+Parse backprop data for regression sets - Elijah
+Multithread - Alex
+ */
+
 public class Driver extends Thread//extending Thread allows for multithreading
 {
-	String fileStart = "dataSets/", fileEnd = ".data", filePath;
+	String fileStart = "dataSets/", fileEnd = ".data", filePath, dataPath;
+	int hiddenLayers, fold;
 	
-	public Driver(String filePath)
+	public Driver(String filePath, int hiddenLayers, int fold)
 	{
 		this.filePath = filePath;
+		dataPath = filePath;
+		this.hiddenLayers = hiddenLayers;
+		this.fold = fold;
 	}
 	
 	public void run() //the method that is called when a Thread starts
@@ -22,6 +41,7 @@ public class Driver extends Thread//extending Thread allows for multithreading
 		{//since each dataset is different it needs its own parse function
 			case "abalone" -> {
 				nodes = p.abaloneParser(fileStart + filePath + fileEnd);
+				filePath +=  "_" + hiddenLayers + "_" + fold;
 				isRegression = true;
 				Printer.println(filePath, "Done Abalone");
 			}
@@ -31,6 +51,7 @@ public class Driver extends Thread//extending Thread allows for multithreading
 			}
 			case "forestfires" -> {
 				nodes = p.firesParser(fileStart + filePath + fileEnd);
+				filePath +=  "_" + hiddenLayers + "_" + fold;
 				isRegression = true;
 				Printer.println(filePath, "Done Forest Fires");
 			}
@@ -40,6 +61,7 @@ public class Driver extends Thread//extending Thread allows for multithreading
 			}
 			case "machine" -> {
 				nodes = p.machineParser(fileStart + filePath + fileEnd);
+				filePath +=  "_" + hiddenLayers + "_" + fold;
 				isRegression = true;
 				Printer.println(filePath, "Done Machine");
 			}
@@ -116,82 +138,77 @@ public class Driver extends Thread//extending Thread allows for multithreading
 		double[] learningRates = new double[] {0.001, 0.01, 0.1, 1};
 		double[] momentums = new double[] {0, 0.001, 0.01, 0.1, 1};    // includes 0 for no momentum
 		
-		for(int layers = 0; layers <= 2; layers++)
+		int layers = hiddenLayers;
+		
+		TrainingGroups groups = new TrainingGroups(dataset);
+		double totalMSE = 0;
+		// Tuning phase
+		ArrayList<Node> tuningSet = groups.getTuningSet();
+		ArrayList<Node> trainingSet = groups.getTrainingSet();
+		RunWithTuning tuner = new RunWithTuning(50, 1000, tuningSet, trainingSet, learningRates, momentums, classes, !isRegression, layers, filePath);
+		System.out.println("Started tuning\t\t" + filePath + "\tfold " + fold + " layer " + layers);
+		tuner.tune();
+		System.out.println("Finished tuning\t\t" + filePath + "\tfold " + fold + " layer " + layers);
+		
+		double learningRate = tuner.getBestLearningRate();
+		double momentum = tuner.getBestMomentum();
+		int[] hiddenLayerNodeNums = tuner.getBestNumNodesPerLayer();
+		
+		// Test phase
+		ArrayList<Node> testSet = groups.getTestSet();
+		Network net = new Network(dataset.get(0).getData().length, hiddenLayerNodeNums, classes, !isRegression);
+		BackPropagation bp = new BackPropagation(net, 10000, learningRate, momentum, filePath);
+		
+		System.out.println("Started training\t" + filePath + "\tfold " + fold + " layer " + layers);
+		bp.trainNetwork(trainingSet);
+		System.out.println("Finished training\t" + filePath + "\tfold " + fold + " layer " + layers);
+		Printer.println(filePath, "\nFold " + fold + " | Number of Hidden Layers: " + layers);
+		Printer.println(filePath, "Number of nodes per hidden layer: " + Arrays.toString(hiddenLayerNodeNums));
+		Printer.println(filePath, "Learning Rate: " + learningRate + " | Momentum Constant: " + momentum);
+		Printer.println(filePath, "Fold " + fold);
+		
+		if(isRegression)
 		{
-			TrainingGroups groups = new TrainingGroups(dataset);
-			double totalMSE = 0;
-			for(int fold = 0; fold < 10; fold++)
+			for(Node node : testSet)
 			{
-				// Tuning phase
-				ArrayList<Node> tuningSet = groups.getTuningSet();
-				ArrayList<Node> trainingSet = groups.getTrainingSet();
-				RunWithTuning tuner = new RunWithTuning(50, 1000, tuningSet, trainingSet, learningRates, momentums, classes, !isRegression, layers, filePath);
-				System.out.println("Started tuning\t\t" + filePath + "\tfold " + fold + " layer " + layers);
-				tuner.tune();
-				System.out.println("Finished tuning\t\t" + filePath + "\tfold " + fold + " layer " + layers);
-				
-				double learningRate = tuner.getBestLearningRate();
-				double momentum = tuner.getBestMomentum();
-				int[] hiddenLayerNodeNums = tuner.getBestNumNodesPerLayer();
-				
-				// Test phase
-				ArrayList<Node> testSet = groups.getTestSet();
-				Network net = new Network(dataset.get(0).getData().length, hiddenLayerNodeNums, classes, !isRegression);
-				BackPropagation bp = new BackPropagation(net, 10000, learningRate, momentum, filePath);
-				
-				System.out.println("Started training\t" + filePath + "\tfold " + fold + " layer " + layers);
-				bp.trainNetwork(trainingSet);
-				System.out.println("Finished training\t" + filePath + "\tfold " + fold + " layer " + layers);
-				Printer.println(filePath, "\nFold " + fold + " | Number of Hidden Layers: " + layers);
-				Printer.println(filePath, "Number of nodes per hidden layer: " + Arrays.toString(hiddenLayerNodeNums));
-				Printer.println(filePath, "Learning Rate: " + learningRate + " | Momentum Constant: " + momentum);
-				Printer.println(filePath, "Fold " + fold);
-				
-				if(isRegression)
+				ArrayList<Neuron> output = net.feedForward(node.getData());
+				for(Neuron neuron : output)
 				{
-					for(Node node : testSet)
-					{
-						ArrayList<Neuron> output = net.feedForward(node.getData());
-						for(Neuron neuron : output)
-						{
-							Printer.println(filePath, "Output: " + neuron.getValue() + " | Original: " + node.getId());
-						}
-					}
+					Printer.println(filePath, "Output: " + neuron.getValue() + " | Original: " + node.getId());
 				}
-				else
-				{
-					HashMap<Neuron, Double> classMap = net.getOutputToClass();
-					for(Node node : testSet)
-					{
-						ArrayList<Neuron> output = net.feedForward(node.getData());
-						Printer.println(filePath, "\nFor example with class " + node.getId() + ":");
-						double highestValue = 0;
-						double mostLikelyClass = 0;
-						for(Neuron neuron : output)
-						{
-							double neuronClass = classMap.get(neuron);
-							double neuronValue = neuron.getValue();
-							Printer.println(filePath, "Output from Neuron corresponding to class " + neuronClass + ": " + neuronValue);
-							if(neuronValue > highestValue)
-							{    // find most likely class
-								highestValue = neuronValue;
-								mostLikelyClass = neuronClass;
-							}
-						}
-						Printer.println(filePath, "Predicted class: " + mostLikelyClass);
-					}
-				}
-				
-				double MSE = bp.calculateMSError(testSet);
-				totalMSE += MSE;
-				Printer.println(filePath, "Overall Mean-Squared Error for Fold " + fold + ": " + MSE + "\n");
-				
-				groups.iterateTestSet();    // Move to next fold
 			}
-			totalMSE /= 10;    // take average
-			Printer.println(filePath, "Average Mean-Squared Error for " + layers + " hidden layers: " + totalMSE + "\n");
 		}
+		else
+		{
+			HashMap<Neuron, Double> classMap = net.getOutputToClass();
+			for(Node node : testSet)
+			{
+				ArrayList<Neuron> output = net.feedForward(node.getData());
+				Printer.println(filePath, "\nFor example with class " + node.getId() + ":");
+				double highestValue = 0;
+				double mostLikelyClass = 0;
+				for(Neuron neuron : output)
+				{
+					double neuronClass = classMap.get(neuron);
+					double neuronValue = neuron.getValue();
+					Printer.println(filePath, "Output from Neuron corresponding to class " + neuronClass + ": " + neuronValue);
+					if(neuronValue > highestValue)
+					{    // find most likely class
+						highestValue = neuronValue;
+						mostLikelyClass = neuronClass;
+					}
+				}
+				Printer.println(filePath, "Predicted class: " + mostLikelyClass);
+			}
+		}
+		
+		double MSE = bp.calculateMSError(testSet);
+		Printer.println(filePath, "Overall Mean-Squared Error for Fold " + fold + ": " + MSE + "\n");
+		
+		//groups.iterateTestSet();    // Move to next fold
 	}
+	
+	
 	
 	public static void main(String[] args)
 	{
@@ -200,11 +217,17 @@ public class Driver extends Thread//extending Thread allows for multithreading
 		test.start();*/
 		
 		//use these if you want to run all the data sets
-		String[] files = {"abalone", "breast-cancer-wisconsin", "forestfires", "glass", "machine", "soybean-small"};
+		String[] files = {"abalone", "forestfires", "machine"};//{"abalone", "breast-cancer-wisconsin", "forestfires", "glass", "machine", "soybean-small"};
 		for(String file : files)//create a new instance of the driver for each of the data sets.
 		{
-			Driver d = new Driver(file);
-			d.start();//Starts a new thread
+			for(int layer = 0; layer < 3; layer++)
+			{
+				for(int fold = 0; fold < 10; fold++)
+				{
+					Driver d = new Driver(file, layer, fold);
+					d.start();//Starts a new thread
+				}
+			}
 		}
 	}
 }
