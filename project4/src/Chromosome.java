2606af6ea35d6a5fdb3ea3cb02f5c02fd4c14cae
Alex Ackerlund <alexackerlund@bresnan.net>
2020-11-17 08:11:13 -0700

    minor modification to code

--- project4/src/Chromosome.java
+++ project4/src/Chromosome.java
@@ -1,4 +1,14 @@
 public class Chromosome {
     public double[] weights;
     public double fitness;
+
+    public Chromosome(int weightSize)
+    {
+        weights = new double[weightSize];
+    }
+
+    public Chromosome(double[] weights)
+    {
+        this.weights = weights;
+    }
 }
--- project4/src/Genetic.java
+++ project4/src/Genetic.java
@@ -1,10 +1,8 @@
 public class Genetic {
     public void runGenetic(Chromosome[] population) {
         Selection s = new Selection();
-        Chromosome parent1 = new Chromosome();
-        Chromosome parent2 = new Chromosome();
-        parent1 = s.tournamentSelection(population);
-        parent2 = s.tournamentSelection(population);
+        Chromosome parent1 = s.tournamentSelection(population);
+        Chromosome parent2 = s.tournamentSelection(population);
         while(parent1 == parent2){
             parent2 = s.tournamentSelection(population);
         }
