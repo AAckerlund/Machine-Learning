90bbb90db1227db83b1044da56f033c14893c575
Alex Ackerlund <alexackerlund@bresnan.net>
2020-11-21 20:56:29 -0700

    implemented PSO tunning

--- project4/src/GATuner.java
+++ project4/src/GATuner.java
@@ -59,6 +59,7 @@ public class GATuner extends Tuner
                             bestMutationRate = mutationRate;
                             bestVariance = variance;
                             bestK = i;
+                            bestError = error;
                         }
                     }
                 }
--- project4/src/PSOTuner.java
+++ project4/src/PSOTuner.java
@@ -1,18 +1,80 @@
 public class PSOTuner extends Tuner
 {
+    private double bestError;
+    private final int maxIterations;
+    private final int numValues;
+
     private int bestParticleCount;
+    private final int maxParticleCount;
+
     private double bestInertia;
+    private double[] inertias;
+
     private double bestCogBias;
+    private double[] cogBiases;
+
     private double bestSocialBias;
+    private double[] socialBiases;
 
-    public PSOTuner()
+    public PSOTuner(int particleCount, double[] inertias, double[] cogBiases, double[] socialBiases, int maxIterations, int numValues)
     {
+        this.numValues = numValues;
+        this.maxIterations = maxIterations;
+        maxParticleCount = particleCount;
 
+        this.inertias = inertias;
+        this.cogBiases = cogBiases;
+        this.socialBiases = socialBiases;
+
+        bestError = Double.MAX_VALUE;
     }
 
     @Override
     void tune()
     {
+        PSO pso;
+        for(double inertia : inertias)
+        {
+            for(double cogBias : cogBiases)
+            {
+                for(double socialBias : socialBiases)
+                {
+                    for(int i = 1; i < maxParticleCount; i++)
+                    {
+                        pso = new PSO(numValues, maxIterations, i, inertia, cogBias, socialBias);
+                        pso.train();
+                        double error = pso.calcMSE();
+                        if(error < bestError)
+                        {
+                            bestError = error;
+                            bestCogBias = cogBias;
+                            bestInertia = inertia;
+                            bestParticleCount = i;
+                            bestSocialBias = socialBias;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
+    public int getBestParticleCount()
+    {
+        return bestParticleCount;
+    }
+
+    public double getBestInertia()
+    {
+        return bestInertia;
+    }
+
+    public double getBestCogBias()
+    {
+        return bestCogBias;
+    }
+
+    public double getBestSocialBias()
+    {
+        return bestSocialBias;
     }
 }
