e08467738c7184e8cb792e811ee5e00eb0d131d8
Alex Ackerlund <alexackerlund@bresnan.net>
2020-11-22 15:27:09 -0700

    DE tuning is implemented

--- project4/src/DETuner.java
+++ project4/src/DETuner.java
@@ -2,17 +2,68 @@ import java.util.ArrayList;
 
 public class DETuner extends Tuner
 {
+    private final ArrayList<Chromosome> weights;
+    private double bestError;
+    
     private double bestCrossoverRate;
-    private double bestMutationRate;
+    private double[] crossoverRates;
+    
     private double bestBeta;
-
-    public DETuner()
+    private double[] betas;
+    
+    private int inputLayerNodeNum;
+    private int[] hiddenLayerNodeNums;
+    private double[] outputLayerClasses;
+    private boolean isClassification;
+    
+    public DETuner(ArrayList<Chromosome> weights, double[] crossoverRates, double[] betas, int inputLayerNodeNum, int[] hiddenLayerNodeNums, double[] outputLayerClasses, boolean isClassification)
     {
-
+        this.weights = weights;
+        bestError = Double.MAX_VALUE;
+        
+        this.crossoverRates = crossoverRates;
+        this.betas = betas;
+        
+        this.inputLayerNodeNum = inputLayerNodeNum;
+        this.hiddenLayerNodeNums = hiddenLayerNodeNums;
+        this.outputLayerClasses = outputLayerClasses;
+        this.isClassification = isClassification;
     }
     @Override
     void tune(ArrayList<Node> trainingSet, ArrayList<Node> tuningSet)
     {
-
+        Trainer DE;
+        double error;
+        Network net = new Network(inputLayerNodeNum, hiddenLayerNodeNums, outputLayerClasses, isClassification);
+        for(double crossoverRate: crossoverRates)
+        {
+            for(double beta : betas)
+            {
+                DE = new DifferentialEvolution(weights, beta, crossoverRate);
+                DE.train(trainingSet);
+                error = net.calculateMSError(tuningSet);
+                if(error < bestError)
+                {
+                    bestCrossoverRate = crossoverRate;
+                    bestBeta = beta;
+                    bestError = error;
+                }
+            }
+        }
+    }
+    
+    public double getBestCrossoverRate()
+    {
+        return bestCrossoverRate;
+    }
+    
+    public double getBestMutationRate()
+    {
+        return bestMutationRate;
+    }
+    
+    public double getBestBeta()
+    {
+        return bestBeta;
     }
 }
